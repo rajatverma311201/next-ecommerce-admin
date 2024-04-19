a024bb5e4167778afeb860ba81c2a5f4bd5460ed
Rajat Verma <rajat311201@gmail.com>
2024-04-19 21:42:28 +0530

    ✅ overview

---
+++ components/overview.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis } from "recharts";
+
+interface OverviewProps {
+    data: any[];
+}
+
+export const Overview: React.FC<OverviewProps> = ({ data }) => {
+    return (
+        <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={data}>
+                <XAxis
+                    dataKey="name"
+                    stroke="#888888"
+                    fontSize={12}
+                    tickLine={false}
+                    axisLine={false}
+                />
+                <YAxis
+                    stroke="#888888"
+                    fontSize={12}
+                    tickLine={false}
+                    axisLine={false}
+                    tickFormatter={(value) => `$${value}`}
+                />
+                <Bar
+                    dataKey="total"
+                    className="fill-indigo-600 dark:fill-indigo-500"
+                    radius={[4, 4, 0, 0]}
+                />
+            </BarChart>
+        </ResponsiveContainer>
+    );
+};
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -15,11 +15,7 @@ export function ThemeToggle() {
 
     return (
         <>
-            <Button
-                size="icon"
-                onClick={handleChangeTheme}
-                className="text-black"
-            >
+            <Button size="icon" onClick={handleChangeTheme} variant={"outline"}>
                 <Sun
                     id="icon-sun-mode-toggle"
                     className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0 "
